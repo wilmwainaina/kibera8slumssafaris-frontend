4a5b8a8302625a38acb0012a89161bb02477ee93
karanja03 <wambuik03@gmail.com>
2023-11-06 00:45:44 +0300

    Added more code to configure tailwind

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+//** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'gradient-027884': 'linear-gradient(90deg, #027884 0%, #FFFFFF 100%)',
+      }),
+      backgroundColor: {
+        'custom': '#027884',
+      },
+    },
   },
   plugins: [],
 }
